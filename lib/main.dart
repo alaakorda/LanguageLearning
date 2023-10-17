d934801c837ce2ea701d3f1144dc19fd7297e3f7
alaakorda <alaazakaria436@gmail.com>
2023-10-17 22:34:05 +0200

    first screen

--- lib/main.dart
+++ lib/main.dart
@@ -10,70 +10,58 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-       
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-    
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
    
-    return Scaffold(
-      appBar: AppBar(
-       
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        
-        title: Text(widget.title),
-      ),
-      body: Center(
-       
-        child: Column(
-         
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
+    return MaterialApp(
+       debugShowCheckedModeBanner: false,
+        home: Scaffold(
+           appBar: AppBar(
+            backgroundColor: Color(0xFF4A322B),
+              title: Text('Toku'),
+          ),
+          body: Column(
+            children: [
+              Container(
+                alignment: Alignment.centerLeft,
+                padding: EdgeInsets.only(left: 16),
+                width: double.infinity,
+                height: 65,
+                color : Color(0XFFFA9532),
+                child: Text('Members',style: TextStyle(fontSize: 18,color: Colors.white,),),
+
+              ),
+               Container(
+                alignment: Alignment.centerLeft,
+                padding: EdgeInsets.only(left: 16),
+                width: double.infinity,
+                height: 65,
+                color : Color(0XFF5A8345),
+                child: Text('Family Members',style: TextStyle(fontSize: 18,color: Colors.white,),),
+
+              ),
+               Container(
+                alignment: Alignment.centerLeft,
+                padding: EdgeInsets.only(left: 16),
+                width: double.infinity,
+                height: 65,
+                color : Color(0XFF7E3FA3),
+                child: Text('Colors',style: TextStyle(fontSize: 18,color: Colors.white,),),
+
+              ),
+               Container(
+                alignment: Alignment.centerLeft,
+                padding: EdgeInsets.only(left: 16),
+                width: double.infinity,
+                height: 65,
+                color : Color(0XFF48A5CC),
+                child: Text('Phrases',style: TextStyle(fontSize: 18,color: Colors.white,),),
+
+              ),
+            ],
+          ),
+
+        )
     );
   }
 }
+
+

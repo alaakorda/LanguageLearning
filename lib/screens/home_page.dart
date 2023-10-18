810c15de2470acf471b562f24a4cb6ec943094bf
alaakorda <alaazakaria436@gmail.com>
2023-10-18 03:13:33 +0200

    second screen

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:languagelearning/components/category_item.dart';
+import 'package:languagelearning/screens/numbers_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -13,7 +14,11 @@ class HomeScreen extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Category('Members', Color(0XFFFA9532), () {}),
+          Category('Members', Color(0XFFFA9532), () {
+            Navigator.push(context, MaterialPageRoute(builder: (context){
+          return NumbersScreen();
+            }));
+          }),
           Category('Family Members',Color(0XFF5A8345), () {}),
           Category('Colors', Color(0XFF7E3FA3), () {}),
           Category('Phrases', Color(0XFF48A5CC), () {}),
---
+++ lib/screens/numbers_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+class NumbersScreen extends StatelessWidget {
+const NumbersScreen({ Key? key }) : super(key: key);
+@override
+Widget build(BuildContext context) {
+return Container(
+
+
+);
+}
+}

b831421c7d77d9e14ea25fe6aee2080ae6d51fab
alaakorda <alaazakaria436@gmail.com>
2023-10-21 02:02:30 +0200

    refactoring

--- lib/screens/numbers_screen.dart
+++ lib/screens/numbers_screen.dart
@@ -3,11 +3,49 @@ import 'package:languagelearning/components/item.dart';
 import 'package:languagelearning/models/numbers.dart';
 
 class NumbersScreen extends StatelessWidget {
-    NumbersScreen({Key? key}) : super(key: key);
-  final Numbers one =
-     Numbers(image: 'assets/images/numbers/number_one.png',
-      JpNum: 'ichi',
-      EnNum: 'one');
+  NumbersScreen({Key? key}) : super(key: key);
+  final List<Numbers> numbers = [
+    Numbers(
+        image: 'assets/images/numbers/number_one.png',
+        JpNum: 'Ichi',
+        EnNum: 'One'),
+    Numbers(
+        image: 'assets/images/numbers/number_two.png',
+        JpNum: 'Ni',
+        EnNum: 'Two'),
+    Numbers(
+        image: 'assets/images/numbers/number_three.png',
+        JpNum: 'San',
+        EnNum: 'Three'),
+    Numbers(
+        image: 'assets/images/numbers/number_four.png',
+        JpNum: 'Shi',
+        EnNum: 'Four'),
+    Numbers(
+        image: 'assets/images/numbers/number_five.png',
+        JpNum: 'Go',
+        EnNum: 'Five'),
+    Numbers(
+        image: 'assets/images/numbers/number_six.png',
+        JpNum: 'Roku',
+        EnNum: 'Six'),
+    Numbers(
+        image: 'assets/images/numbers/number_seven.png',
+        JpNum: 'Sebun',
+        EnNum: 'Seven'),
+    Numbers(
+        image: 'assets/images/numbers/number_eight.png',
+        JpNum: 'Hachi',
+        EnNum: 'Eight'),
+    Numbers(
+        image: 'assets/images/numbers/number_nine.png',
+        JpNum: 'Kyu',
+        EnNum: 'Nine'),
+    Numbers(
+        image: 'assets/images/numbers/number_ten.png',
+        JpNum: 'Ju',
+        EnNum: 'Ten'),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,14 +53,20 @@ class NumbersScreen extends StatelessWidget {
         title: Text('Numbers'),
         backgroundColor: Color(0xFF4A322B),
       ),
-      body:
-       Column(
-         children: [
-           Item(number: one),
-         ],
-       ),
+      body: ListView.builder(
+          itemCount: numbers.length,
+          itemBuilder: (context, index) {
+            return Item(
+              number: numbers[index],
+            );
+          }),
     );
   }
+
+  // List<Widget> getList(List<Numbers> numbers) {
+  //   List<Widget> Itemslist = [];
+  //   for (int i = 0; i < numbers.length; i++)
+  //     Itemslist.add(Item(number: numbers[i]));
+  //   return Itemslist;
+  // }
 }
-
-
